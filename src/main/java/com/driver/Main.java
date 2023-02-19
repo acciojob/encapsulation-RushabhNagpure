eb3de8fc05973ade328a8b210133754ef4bf8c91
Rushabh <rushabhnagpure864@gmail.com>
2023-02-19 23:22:07 +0530

    Abstrction is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import static java.lang.System.out;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Rushabh");
+        out.println(obj.getName());
+    }
 }
